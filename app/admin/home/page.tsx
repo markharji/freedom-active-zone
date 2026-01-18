9594de9fc56458b5a9156411d4afbbb7062b5553
Mark Harji Mating <mark@nutshelltechnology.com.au>
2026-01-18 20:44:59 +0800

    fix deployment api

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -12,7 +12,7 @@ export default function AdminHomePage() {
   const [apparelTransactions, setApparelTransactions] = useState([]);
 
   const fetchFacilityTransactions = async () => {
-    const res = await fetch(`api/facility-transactions`, {
+    const res = await fetch(`/api/facility-transactions`, {
       cache: "no-store",
     });
     if (!res.ok) throw new Error("Failed to fetch facility transactions");
@@ -22,8 +22,7 @@ export default function AdminHomePage() {
 
   // Fetch apparel transactions
   const fetchApparelTransactions = async () => {
-    console.log(`api/apparel-transactions`);
-    const res = await fetch(`api/apparel-transactions`);
+    const res = await fetch(`/api/apparel-transactions`);
     if (!res.ok) throw new Error("Failed to fetch apparel transactions");
     const data = await res.json();
     setApparelTransactions(data);
